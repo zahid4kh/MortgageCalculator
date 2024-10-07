73b965a98287a6bf1da79287b4fa6124cccaa4a5
zahid4kh <halilzahid@gmail.com>
2024-10-07 21:44:39 +0200

    add more color schemes

--- composeApp/src/commonMain/kotlin/calculate/mortgage/Colors.kt
+++ composeApp/src/commonMain/kotlin/calculate/mortgage/Colors.kt
@@ -1,5 +1,18 @@
 package calculate.mortgage
 
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import org.jetbrains.skia.ColorSpace.Companion.sRGB
+
+@Composable
+fun backgroundColor(): Color{
+    val bkg = if (isSystemInDarkTheme()){
+        Color.Black
+    }else{
+        Color.White
+    }
+    return bkg
+}
+
 
