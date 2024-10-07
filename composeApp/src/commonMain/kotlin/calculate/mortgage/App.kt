099db2f8471fe1bc324a483a676cfc7ddfe1f4f0
zahid4kh <halilzahid@gmail.com>
2024-10-07 20:43:38 +0200

    ui -> TODO: calculation and show resultas

--- composeApp/src/commonMain/kotlin/calculate/mortgage/App.kt
+++ composeApp/src/commonMain/kotlin/calculate/mortgage/App.kt
@@ -1,37 +1,70 @@
 package calculate.mortgage
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import mortgagecalculator.composeapp.generated.resources.Res
-import mortgagecalculator.composeapp.generated.resources.compose_multiplatform
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-@Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
+        Calculator()
+    }
+}
+
+@Composable
+fun Calculator(){
+    var amount by remember { mutableStateOf("") }
+    var term by remember { mutableStateOf("") }
+    var rate by remember { mutableStateOf("") }
+    val smallTextField = 170.dp
+    val bigTextField = smallTextField * 2
+    var isRepayment by remember { mutableStateOf(false) }
+    var isInterestOnly by remember { mutableStateOf(false) }
+
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center)
+    {
+        Header()
+
+        OutlinedTextField(value = amount, onValueChange = {amount = it}, label = {Text (mortgageAmount)}, modifier = Modifier.width(bigTextField))
+
+        Row(){
+            OutlinedTextField(value = term, onValueChange = {term = it}, label = {Text (mortgageTerm)}, modifier = Modifier.width(smallTextField))
+            Spacer(modifier = Modifier.width(5.dp))
+            OutlinedTextField(value = rate, onValueChange = {rate = it}, label = {Text (interestRate)}, modifier = Modifier.width(smallTextField))
         }
+
+        Text(text = mortgageType, modifier = Modifier.align(alignment = Alignment.CenterHorizontally).padding(top = 12.dp))
+
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.width(bigTextField)){
+            Checkbox(checked = isRepayment, onCheckedChange = {isRepayment = it; isInterestOnly = false})
+            Text(text = repayment)
+        }
+        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.width(bigTextField)){
+            Checkbox(checked = isInterestOnly, onCheckedChange = {isInterestOnly = it; isRepayment = false})
+            Text(text = interestOnly)
+        }
+    }
+}
+
+@Composable
+fun Header(){
+    Row(modifier = Modifier.padding(top = 10.dp, bottom = 20.dp), horizontalArrangement = Arrangement.Center){
+        Text (text = title, fontWeight = FontWeight.Bold)
     }
 }
---
+++ composeApp/src/commonMain/kotlin/calculate/mortgage/Strings.kt
@@ -0,0 +1,16 @@
+package calculate.mortgage
+
+
+val title = "Mortgage Calculator"
+val clearAll = "Clear All"
+
+val mortgageAmount = "Mortgage Amount"
+val mortgageTerm = "Mortgage Term"
+val interestRate = "Interest Rate"
+val years = "years"
+
+val mortgageType = "Mortgage Type"
+val repayment = "Repayment"
+val interestOnly = "Interest Only"
+
+val calculate = "Calculate Repayments"
