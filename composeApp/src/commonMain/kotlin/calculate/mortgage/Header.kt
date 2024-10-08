136cac2926e9041b90f385a812f0ceff440b787c
zahid4kh <halilzahid@gmail.com>
2024-10-08 23:48:29 +0200

    added dynamic colors

--- composeApp/src/commonMain/kotlin/calculate/mortgage/Header.kt
+++ composeApp/src/commonMain/kotlin/calculate/mortgage/Header.kt
@@ -6,14 +6,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 
 @Composable
-fun Header(){
+fun Header(txtColor: Color){
     Row(modifier = Modifier.padding(top = 10.dp, bottom = 20.dp), horizontalArrangement = Arrangement.Center){
-        Text (text = title, fontWeight = FontWeight.Bold, fontSize = 30.sp)
+        Text (text = title, fontWeight = FontWeight.Bold, fontSize = 30.sp, color = txtColor)
     }
 }
