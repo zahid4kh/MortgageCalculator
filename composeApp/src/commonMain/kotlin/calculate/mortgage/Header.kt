e6858fea6a3fe8db4f89f84da61b928da2b144b6
zahid4kh <halilzahid@gmail.com>
2024-10-08 11:21:55 +0200

    separated header file

---
+++ composeApp/src/commonMain/kotlin/calculate/mortgage/Header.kt
@@ -0,0 +1,19 @@
+package calculate.mortgage
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun Header(){
+    Row(modifier = Modifier.padding(top = 10.dp, bottom = 20.dp), horizontalArrangement = Arrangement.Center){
+        Text (text = title, fontWeight = FontWeight.Bold, fontSize = 30.sp)
+    }
+}
