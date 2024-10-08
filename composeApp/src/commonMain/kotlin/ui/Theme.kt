cc1090d01c8e8b70444115bbd1f9acd84e3ccdb4
zahid4kh <halilzahid@gmail.com>
2024-10-08 19:30:30 +0200

    adding dynamic colors

--- composeApp/src/commonMain/kotlin/ui/Theme.kt
+++ composeApp/src/commonMain/kotlin/ui/Theme.kt
@@ -1,16 +1,13 @@
 package ui
-import android.app.Activity
-import android.os.Build
+
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
+
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
@@ -87,39 +84,3 @@ private val darkScheme = darkColorScheme(
     surfaceContainerHigh = surfaceContainerHighDark,
     surfaceContainerHighest = surfaceContainerHighestDark,
 )
-
-@Immutable
-data class ColorFamily(
-    val color: Color,
-    val onColor: Color,
-    val colorContainer: Color,
-    val onColorContainer: Color
-)
-
-val unspecified_scheme = ColorFamily(
-    Color.Unspecified, Color.Unspecified, Color.Unspecified, Color.Unspecified
-)
-
-@Composable
-fun MaterialTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-
-    dynamicColor: Boolean = true,
-    content: @Composable() () -> Unit
-) {
-  val colorScheme = when {
-      dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-          val context = LocalContext.current
-          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-      }
-      
-      darkTheme -> darkScheme
-      else -> lightScheme
-  }
-
-  MaterialTheme(
-    colorScheme = colorScheme,
-    content = content
-  )
-}
-
